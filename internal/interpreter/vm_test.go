6424ce8e22472b09585004daf6da9113ce409f42
Gabriele <gabrielecabriniii@gmail.com>
2025-09-02 08:25:04 +0200

    test: fix vm_test benchmarks

--- internal/interpreter/vm_test.go
+++ internal/interpreter/vm_test.go
@@ -6,8 +6,26 @@ import (
 	"testing"
 )
 
-func BenchmarkVM(b *testing.B) {
-	data, err := os.ReadFile("../examples/fibonacci.b")
+func BenchmarkUnoptimizedVM(b *testing.B) {
+	data, err := os.ReadFile("../../examples/fibonacci.b")
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	instrs, err := parser.ParseSourceBytes(data)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	vm := VM{}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		vm.Execute(instrs)
+	}
+}
+
+func BenchmarkOptimizedVM(b *testing.B) {
+	data, err := os.ReadFile("../../examples/fibonacci.b")
 	if err != nil {
 		b.Fatal(err)
 	}
